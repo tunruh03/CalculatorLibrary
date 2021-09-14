f4031c2bf595618972acb06a9be218917557cd52
tunruh03 <tunruh01@gmail.com>
2021-09-14 01:55:01 -0600

    adding 2 of 4 functions to test first

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+#the first step in testing is to lint the code which checks for possible
+#syntactical errors
+
+#the second step is unit testing
+#unit testing is designed to check a single function or unit of code
+
+#another standard that goes hand in hand with testing is called code
+#coverage. coverage is the percentage of the source code that is
+#covered by tests
